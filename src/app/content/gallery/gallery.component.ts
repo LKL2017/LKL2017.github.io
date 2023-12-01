35ace57f9797fd284e7c12d610782a6815bb804e
kunlinl <scu2018lkl@gmail.com>
2023-12-01 23:56:02 +0800

    chore: add gallery placeholder

--- src/app/content/gallery/gallery.component.ts
+++ src/app/content/gallery/gallery.component.ts
@@ -1,13 +1,30 @@
 import {CommonModule} from "@angular/common";
 import { Component } from '@angular/core';
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+
+interface GalleryWork {
+  title: string;
+  subTitle: string;
+  description: string;
+  previewSrc: string;
+}
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.less',
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule, MatCardModule, MatButtonModule]
 })
 export class GalleryComponent {
 
+  works: GalleryWork[] = [
+    { title: 'effect.ts', subTitle: 'p5.js', previewSrc: '', description: new Array(20).fill('TEST').join(' ') },
+    { title: 'effect.ts', subTitle: 'vanilla', previewSrc: '', description: new Array(30).fill('TEST').join(' ') },
+    { title: 'effect.ts', subTitle: 'p5.js', previewSrc: '', description: new Array(40).fill('TEST').join(' ') },
+  ];
+
+  constructor() {
+  }
 }
